e19569a17d152cf4449abb46cd6faceb2e49eacd
Mitchel Parish <mitchelparish@gmail.com>
2019-01-25 22:04:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,7 +4,10 @@ class Anagram
   def initialize(word)
     @word = word
   end
-  
 
-
+  def match(array)
+    array.select do |x|
+      (@word.split("").sort) == (x.split("").sort)
+    end
+  end
 end
